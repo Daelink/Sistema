e0ee44c5398e12f507c4c86ceda3badbc22d7859
Daelink <daelinkfirebase@gmail.com>
2024-10-03 21:51:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def recommend_profile():
 
     return jsonify(recommendations)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+   if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
