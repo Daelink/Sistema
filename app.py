bb51217ea0581a902a81877844ccbd5aa4ae45c2
Daelink <daelinkfirebase@gmail.com>
2024-10-03 21:16:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,19 @@ app = Flask(__name__)
 CORS(app)
 
 # Configurar o Firebase
-cred_path = os.getenv('FIREBASE_CREDENTIALS')
+cred = credentials.Certificate({
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),  # Corrige a quebra de linha
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+    "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+})
 
 if not os.path.exists(cred_path):
     print("Arquivo de credenciais não encontrado. Verifique o caminho.")
